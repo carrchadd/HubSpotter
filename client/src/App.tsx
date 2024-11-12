3163c620c0b5dfa930f6b72c98f54a3981f2b8fe
Chad <107705900+carrchadd@users.noreply.github.com>
2024-11-12 00:20:31 -0500

    Adding login UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import LandingPage from './components/LandingPage'
+// import LandingPage from './components/LandingPage'
 import LogIn from './components/LogIn';
 
 function App() {
