e1ded796dd6e60223e63859707d2def77625db3e
Chad <107705900+carrchadd@users.noreply.github.com>
2024-11-15 01:34:17 -0500

    Adding server side dependencies & configurations

--- server/test/env.test.js
+++ server/test/env.test.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv");
+require('dotenv').config();
+
 describe('Retrieving Environment Variables', () => {
   it('should have MONGO_URI and PORT defined in environment variables', () => {
     expect(process.env.MONGO_URI).toBeDefined();
